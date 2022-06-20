a7b2badd77805d88cb4778c4b5b2b5e1c6bef6e0
rchacoma8 <rchacoma8@alumnes.ub.edu>
2022-06-20 19:28:58 +0200

    Include Bootstrap and Bootstrap Icons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Icon:
+          <i class="bi bi-heart-pulse-fill"></i>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
